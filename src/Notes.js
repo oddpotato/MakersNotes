31e41498e316336e71a1089f851d7a32f48a02eb
Fiona Apps <wikipanyd@gmail.com>
2021-09-06 16:52:23 +0100

    A full cohort of note methods

--- src/Notes.js
+++ src/Notes.js
@@ -1,26 +1,23 @@
-// const Notes = {
-//     
-//     allNotes = Array.new,
-// 
-//     create = function(noteContent) {
-//         allNotes.push(noteContent);
-//     }
-// 
-// }
-// 
-// Notes.create('Hello I am a string that I am thinking is going to be a note')
 
 class NoteList {
 
     constructor(){
         this.everyNote = []
-        console.log(this.everyNote)
     }
 
     addNewNote(noteContent){
-        console.log('Want to add a new note?')
         let note = new Note(noteContent)
-        this.everyNote.push([note.content])
+        this.everyNote.push(note.content)
+    }
+
+    displayAllNotes(){
+        this.everyNote.forEach((note) =>{
+            console.log(note.slice(0,19));
+        })
+    }
+
+    displayNote(index){
+        console.log(this.everyNote[index]);
     }
 }
 
@@ -28,17 +25,15 @@ class Note {
 
     constructor(noteContent){
         this.content = noteContent
-        console.log(noteContent)
     }
 }
 
 
-listOfNotes = new NoteList
-listOfNotes.addNewNote('Hello This is me testing a thing')
-console.log(listOfNotes.everyNote)
-
-listOfNotes.addNewNote('Ignore it')
-listOfNotes.addNewNote('Hi Javascript Gods')
-console.log(listOfNotes.everyNote)
-
-console.log(listOfNotes.everyNote[0])
+// listOfNotes = new NoteList
+// listOfNotes.addNewNote('Hello This is me testing a thing')
+// 
+// listOfNotes.addNewNote('Ignore it')
+// listOfNotes.addNewNote('Hi Javascript Gods')
+// console.log(listOfNotes.everyNote)
+// 
+// listOfNotes.displayAllNotes()
