2e90458ea7a473f96eb7b9d8d713626686d2a183
William Day <williamjday11@gmail.com>
2021-09-06 17:21:33 +0100

    Added basic feed to Interface.js with results in descending order

--- src/Interface.js
+++ src/Interface.js
@@ -1,10 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+  const notes = ["message1", "message2", "message3", "message4", "message5"]
   // This will access Notes class
-  const notesArray = ["message1" , "message2", "message3"]
-
-  const viewNotes = () => {"This is the first test message"
-    document.querySelector('#messageIntro').innerText = notesArray;
+  const viewNotes = () => {
+    let str = '<ul>'
+    notes.reverse().forEach(function(note) {
+      str += '<li>'+ note+ '</li>';
+    }); 
+  str += '</ul>';
+  document.querySelector("#noteContainer").innerHTML = str;
   };
   viewNotes();
 })
