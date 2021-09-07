6ee2149ff07f28848f8b58921f9356067c6fd837
sa-mcquanzie <sa.mcquanzie@gmail.com>
2021-09-07 17:48:10 +0100

    Adding emoji capability to Notes

--- src/Notes.js
+++ src/Notes.js
@@ -1,38 +1,48 @@
 
 class NoteList {
-    constructor(){
-        this.everyNote = []
-    }
+  constructor() {
+      this.everyNote = [];
+  }
 
-    addNewNote(noteContent){
-        let note = new Note(noteContent)
-        this.everyNote.push(note.content)
-    }
+  addNewNote(content) {
+      let newNote = new Note(content);
+      this.everyNote.push(newNote);
+  }
 
-    displayAllNotes(){
-        this.everyNote.forEach((note) =>{
-            console.log(note.slice(0,19));
-        })
-    }
+  displayAllNotes() {
+    let notes = [];
 
-    displayNote(index){
-        console.log(this.everyNote[index]);
-    }
+      this.everyNote.forEach((note) => {
+          notes.push(note.content.slice(0,19));
+      })
+    
+    return notes;
+  }
+
+  displayNote(index) {
+      return this.everyNote[index];
+  }
 }
 
 class Note {
+  constructor(noteContent) {
+      this.content;
 
-    constructor(noteContent){
-        this.content = noteContent
+      let contentJSON = JSON.parse(`{"text":"${noteContent}"}`);
+
+      async function emojify(content) {
+        let response = await fetch('https://makers-emojify.herokuapp.com/', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(content),
+        });
+
+        let data = await response.json();
+        return data.emojified_text;
     }
+
+    emojify(contentJSON).then((value) => this.content = value);   
+  }
 }
-
-
-// listOfNotes = new NoteList
-// listOfNotes.addNewNote('Hello This is me testing a thing')
-// 
-// listOfNotes.addNewNote('Ignore it')
-// listOfNotes.addNewNote('Hi Javascript Gods')
-// console.log(listOfNotes.everyNote)
-// 
-// listOfNotes.displayAllNotes()
